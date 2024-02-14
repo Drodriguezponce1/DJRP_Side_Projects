71eb25b80632c54b15c3e39915b926a172012a79
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-02-14 12:19:26 -0800

    tg

---
+++ Python/NetworkChuckPythonCourse/Episode2.py
@@ -0,0 +1,17 @@
+#Let's start a coffee shop together!! We're going to build a coffee shop using some new Python programming concepts!!
+
+print("Hello There! Please feel free to enjoy the coffee!")
+
+x = input("Please enter name: ")
+y = input("Please enter the coffe: ")
+
+print("Thank you for ordering! " + x + " your " + y + " will be ready in just a moment!")
+
+
+
+
+#CHALLENGE 1 ----> (timestamp 12:36)
+
+#Program the robot barista to give the customer a menu, (make the menu a variabe) ask them what they want, and tell them it will be ready in a moment. Here is an example of the output:
+
+#Sounds good customer_name! We'll have that coffee_beverage ready for you in a moment!!
