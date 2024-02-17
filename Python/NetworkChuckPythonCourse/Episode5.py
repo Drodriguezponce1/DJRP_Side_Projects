aad28dddaaf0faf5dec65326681012bbdcc84dc1
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-02-16 16:23:02 -0800

    adding a dictionary because wanted it more challenging

--- Python/NetworkChuckPythonCourse/Episode5.py
+++ Python/NetworkChuckPythonCourse/Episode5.py
@@ -1,9 +1,9 @@
 # https://www.youtube.com/watch?v=Ec9WQGw4lW0&list=PLIhvC56v63ILPDA2DQBv0IKzqsWTZxCkp&index=5
 x = input("What is your name: ")
-menu = "BLACK COFFEE, ESPRESSO, LATTE, CAPPUCINO, FRAPPUCINO"
+menu = {"BLACK COFFEE" : 3, "JAVA" : 4, "CAPPUCCINO": 8}
 
 if(x == "Ben"):
-    y = input("Hey Ben! Are you evil: ")
+    y = input("Hey "+ x +"! Are you evil: ")
     if(y == "No"):
         print("WELCOME " + x)
     else:
@@ -11,3 +11,18 @@ if(x == "Ben"):
         exit()
 else:
     print("Hello " + x + ", thank you so much for coming in today!")
+
+order = input("Menu: BLACK COFFEE, JAVA, CAPPUCINNO\nPlease enter which item you like: ")
+
+t = True
+
+while(t):
+    if order in menu.keys():
+        print("CHOSEN ITEM: " + order)
+
+        amount = int(input("ENTER THE AMOUNT OF COFFEES YOU WANT: "))
+
+        print("TOTAL AMOUNT: " + str(amount * menu[order]))
+        t = False
+    else:
+        order = input("PLEASE ORDER SOMETHING ON THE MENU: ")
