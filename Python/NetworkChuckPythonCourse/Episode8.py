c25c14d0c4ccf8b8a6c41aa121dc24d55fe47c82
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-02-16 17:49:17 -0800

    personal testing

---
+++ Python/NetworkChuckPythonCourse/Episode8.py
@@ -0,0 +1,12 @@
+
+lsit = []
+
+print("ENTER 7 VALUES: ")
+for i in range(7):
+    lsit.append(int(input("VALUE " + str(i + 1) + ": ")))
+
+print(lsit)
+
+lsit.reverse()
+
+print(lsit)
--- Python/Test/main.py
+++ Python/Test/main.py
@@ -1,9 +1,39 @@
+import os
+
+def main():
+    print(sum(3))
+    stry = "RANDOMSTRING"
+    print(stry[3:-2:2])
+
+    path = r"C:\Users\Daniel\Documents\Side_Projects\DJRP_Side_Projects\Python\Test\tester.txt";
+    path2 = r"C:\Users\Daniel\Documents\Side_Projects\DJRP_Side_Projects\OSRS\BarrowsText.txt"
+    d = readText(path2)
+    print(type(d))
+    print(d)
+
 
 def sum(x):
     return x * 2;
 
 
-print(sum(3))
+def readText(f):
 
-stry = "RANDOMSTRING"
-print(stry[3:-2:2])
+    di = {}
+    text = open(f,"r")
+    counter = 0
+    for x in text:
+        temp = x.split(",")
+        di.update({temp[0]: temp[1][0:-1]})
+
+    text.close();
+    return di;
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
