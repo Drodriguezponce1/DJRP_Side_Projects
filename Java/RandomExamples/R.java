32a402adb519d677a7f1e9125224032ddcb9f2d9
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-02-20 23:22:16 -0800

    test

--- Java/RandomExamples/R.java
+++ Java/RandomExamples/R.java
@@ -2,12 +2,8 @@ package Java.RandomExamples;
 public class R{
 
     public static void main(String[] args) {
-        String s = "asfasdfa";
-
-        for(char c:s.toCharArray())
-        {
-            
-        }
+       
+       
     }
 
 }
