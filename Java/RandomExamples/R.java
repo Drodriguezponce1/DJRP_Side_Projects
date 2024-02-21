65b2aff4438b67d065be3e74736e46a86329e45a
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-02-20 23:21:46 -0800

    adding examples

---
+++ Java/RandomExamples/R.java
@@ -0,0 +1,13 @@
+package Java.RandomExamples;
+public class R{
+
+    public static void main(String[] args) {
+        String s = "asfasdfa";
+
+        for(char c:s.toCharArray())
+        {
+            
+        }
+    }
+
+}
--- Python/NetworkChuckPythonCourse/Episode1.py
+++ Python/NetworkChuckPythonCourse/Episode1.py
@@ -1,3 +1,9 @@
 print("Hello World")
 
 print("This\nis just\npractice\nwith strings")
+
+x,y,z = 1,2,"fafa"
+
+print(x)
+print(y)
+print(z)
