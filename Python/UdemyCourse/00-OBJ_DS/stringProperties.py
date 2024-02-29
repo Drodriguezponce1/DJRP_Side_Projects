43e98740ef88dada60a098a0219790d7bf4d72ad
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-02-29 14:55:16 -0800

    more changes

---
+++ Python/UdemyCourse/00-OBJ_DS/stringProperties.py
@@ -0,0 +1,24 @@
+#change a part of a str
+name = "Sam"
+change = name[1:3]
+newName = "P" + change
+
+print(newName)
+
+#String concatination
+myStr = "Hello"
+myStr += " World"
+
+print(myStr)
+
+#Dynamic typing - adding a integer to a str
+number = 1+2
+
+print("1 + 2 = " + str(number))
+
+#more examples
+    #capitalize the first letter
+x = "hello world"
+x = x.capitalize()
+
+print(x)
