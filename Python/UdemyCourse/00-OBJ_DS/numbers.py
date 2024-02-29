53920c8093694ca6e4dd2e900717514ccd90d06a
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-02-29 14:03:28 -0800

    numbers.py

--- Python/UdemyCourse/00-OBJ_DS/numbers.py
+++ Python/UdemyCourse/00-OBJ_DS/numbers.py
@@ -1,14 +1,18 @@
 #adding
-print(2+1)
+print("2 + 1 = " + str(2+1))
 
 #subtracting
-print(1-5)
+print("1 - 5 = " + str(1-5))
 
 #division
-print(1/2)
+print("1 / 2 = " + str(1/2))
 
 #multiplication
-print(1*7)
+print("1 x 7 = " + str(1*7))
 
 #modolus or remainder
-print(20%3)
+print("20 mod 3 = " + str(20%3))
+
+#power
+print("2 ^ 2 = " + str(2 ** 2))
+
