7e4f4e993d8e7bcc06a8caf5fa22238b14f5d1ae
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-02-29 14:17:04 -0800

    strings.py

---
+++ Python/UdemyCourse/00-OBJ_DS/strings.py
@@ -0,0 +1,19 @@
+# 'Hello' is stored as [H,e,l,l,o]
+st = "Hello"
+
+print(st[0])
+
+#using the double quotations
+temp = "I'M USING A SINGLE QUOTE"
+print(temp)
+
+#escape squences
+    #Tab
+print("Hello \t")
+    #New Line
+print("Hello \nWorld")
+    #Backspace
+print("Hello\bWorld")
+
+#length of a string
+x = len(temp)
