409d71837fbcef302c2fc306d1a45ceb7229ecbf
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-03-08 19:55:05 -0800

    Simple Python Boolean section

---
+++ Python/UdemyCourse/01-Python_Comp/main.py
@@ -0,0 +1,12 @@
+# Return booleans
+print(1 == 2)
+
+#Python does type checking like Java
+print ('1' == 1) # should be false
+
+#chain comparison
+# use and, or, and not
+
+print('h' == 'h' and 2 == 2)
+
+print(1 != 2)
