72480052643a4d1200e2898603544dee73408759
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-03-13 22:06:31 -0700

    functions intro

---
+++ Python/UdemyCourse/03-Python_Methods/Methods/function_intro.py
@@ -0,0 +1,30 @@
+#def means defenition
+
+def intro(x = int):
+    print(x)
+    return 0
+
+def snake_casing(x = str):
+    print(x)
+    return 0
+
+intro("sdfs")
+
+def hello_world():
+    print("Hello World")
+    return 0
+
+hello_world()
+
+def name(n):
+    print(f"Your name is {n}")
+    return 0
+
+name("Daniel")
+
+#example
+def sum(num1, num2):
+    return num1 + num2
+
+x = sum(1,2)
+print(x)
