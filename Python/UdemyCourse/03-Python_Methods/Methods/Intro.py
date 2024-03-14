305f6860fbc7bb57229eab90cb4db4165a72c1d8
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-03-13 21:57:55 -0700

    intro to methods and python docs

---
+++ Python/UdemyCourse/03-Python_Methods/Methods/Intro.py
@@ -0,0 +1,10 @@
+#list methods
+myList = [1,2,3,4,5]
+myList.append(6)
+print(myList)
+
+#to see methods just do ctrl spacebar
+print(myList.index(3))
+
+#for python documentation: https://docs.python.org/3/
+help(myList.append)
