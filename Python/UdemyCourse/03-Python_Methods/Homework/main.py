83d711449014068f594bf5951be24a144da3a4da
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-03-14 23:33:38 -0700

    mian changes

---
+++ Python/UdemyCourse/03-Python_Methods/Homework/main.py
@@ -0,0 +1,4 @@
+# https://github.com/Drodriguezponce1/Complete-Python-3-Bootcamp/blob/master/03-Methods%20and%20Functions/08-Functions%20and%20Methods%20Homework.ipynb
+import math
+
+print(math.pi)
