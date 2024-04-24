66bbaaaddb1ece7b6e88e1f7c74ca9ebc4ed9c5f
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-04-24 13:55:37 -0700

    finished programing the war game

--- Python/UdemyCourse/08-Milestone_Project2/war.py
+++ Python/UdemyCourse/08-Milestone_Project2/war.py
@@ -83,3 +83,73 @@ print(player)
 
 player.remove_one()
 print(player)'''
+
+#Game setup
+player_one = Player("One")
+player_two = Player("Two")
+
+deck = Deck()
+deck.shuffle()
+
+for cards in range(26):
+    player_one.add_cards(deck.deal_one())
+    player_two.add_cards(deck.deal_one())
+
+print(player_one)
+print(player_two)
+
+game_on = True
+
+round = 0
+#while game_on
+
+while game_on:
+
+    round += 1
+    print(f"Starting round {round}!")
+
+    if len(player_one.cards) == 0:
+        print(f"Player One, has ran out of cards! Player Two wins!")
+        game_on = False
+        break
+    elif len(player_two.cards) == 0:
+        print(f"Player Two, has ran out of cards! Player One wins!")
+        game_on = False
+        break
+    
+    #Start a new round
+    #The cards that are in play!
+    player_one_cards = []
+    player_two_cards = []
+
+    player_one_cards.append(player_one.remove_one())
+    player_two_cards.append(player_two.remove_one())
+   
+    at_war = True
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_two_cards)
+            player_two.add_cards(player_one_cards)
+            at_war = False
+        else:
+            print("War!")
+
+            if len(player_one.cards) < 3:
+                print("Player one unable to declare war!")
+                print("Player two wins! ")
+                game_on = False
+                break
+            elif len(player_two.cards) < 3:
+                print("Player two unable to declare war!")
+                print("Player one wins! ")
+                game_on = False
+                break
+            else:
+                for x in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
