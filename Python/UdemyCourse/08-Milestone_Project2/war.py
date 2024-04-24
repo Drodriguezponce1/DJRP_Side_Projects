a641401582ecccedfae699a379ad32ca1916a3dd
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-04-24 12:32:51 -0700

    starting war.py, a war card game

--- Python/UdemyCourse/08-Milestone_Project2/war.py
+++ Python/UdemyCourse/08-Milestone_Project2/war.py
@@ -15,10 +15,40 @@ class Card():
     def __str__(self):
         return f"[Rank: {self.rank}, Suit: {self.suit}]"
 
-
+'''
 two_hearts = Card("Hearts", "two")
 three_hearts = Card("Clubs", "Three")
 print(two_hearts)
 print(two_hearts.value)
 
-print(three_hearts.value > two_hearts.value)
+print(three_hearts.value > two_hearts.value)'''
+
+class Deck():
+
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                # Create a Card object
+                card = Card(suit, rank)
+                self.all_cards.append(card)
+    
+    def shuffle(self):
+       random.shuffle(self.all_cards)
+    
+    def deal_one(self):
+        return self.all_cards.pop()
+    
+new_deck = Deck()
+
+new_deck.shuffle()
+for i,cards in enumerate(new_deck.all_cards):
+    print(f"Card {i}: {cards}")
+
+card = new_deck.deal_one()
+
+for i,cards in enumerate(new_deck.all_cards):
+    print(f"Card {i}: {cards}")
+
+print(card)
