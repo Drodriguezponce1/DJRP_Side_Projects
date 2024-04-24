89d4647f3ce95070968ab368d8056a9a391c9851
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-04-24 11:08:17 -0700

    making a program to play War, a card game

---
+++ Python/UdemyCourse/08-Milestone_Project2/war.py
@@ -0,0 +1,24 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+class Card():
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[self.rank.capitalize()]
+    
+
+    def __str__(self):
+        return f"[Rank: {self.rank}, Suit: {self.suit}]"
+
+
+two_hearts = Card("Hearts", "two")
+three_hearts = Card("Clubs", "Three")
+print(two_hearts)
+print(two_hearts.value)
+
+print(three_hearts.value > two_hearts.value)
