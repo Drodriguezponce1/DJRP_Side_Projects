f5753c34ad97a22d75bcf7d0fe5a8ff87a62d4bf
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-05-16 14:04:29 -0700

    testing Person class

--- Java/Practice/PersonDriver.java
+++ Java/Practice/PersonDriver.java
@@ -1,3 +1,14 @@
 public class PersonDriver {
     
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        System.out.println(p);
+
+        System.out.println(p.equals(p));
+
+        Person d = new Person("Daniel", 26, "Random Address");
+        System.out.println(d);
+        System.out.println(d.equals(p));
+    }
 }
