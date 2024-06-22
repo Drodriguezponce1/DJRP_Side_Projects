0aae27d7a7faa6e945deaa9c40386ad4dd1768be
Drodriguezponce1 <Danieljrodcoding@gmail.com>
2024-06-21 19:05:11 -0700

    from my memory doing binary search

--- Java/RandomExamples/Sorting_Searching.java
+++ Java/RandomExamples/Sorting_Searching.java
@@ -7,7 +7,10 @@ public class Sorting_Searching {
     public static void main(String[] args) {
         int[] a = {3,5,0,1,2,4};
 
-        System.out.println(Arrays.toString(selection_sort(a)));
+        a = selection_sort(a);
+        System.out.println(Arrays.toString(a));
+
+        System.out.println(binary_search(a, 5));
     }
 
     public static int[] selection_sort(int[] a){
@@ -35,5 +38,32 @@ public class Sorting_Searching {
         }
 
         return a;
+    }
+
+    public static int binary_search(int[] a, int n)
+    {
+        //grab the left most index and right most index
+        int left = 0;
+        int right = a.length - 1;
+
+        while(left <= right)
+        {
+            //grab the middle between the left most and right most
+            int middle = left + (right - left) / 2;;
+            if(n == a[middle])
+            {
+                return middle;
+            }
+            else if(n > a[middle])
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
     }
 }
