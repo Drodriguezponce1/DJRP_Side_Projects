570b508e5e15fd2912ef548c7ec04b37246361fa
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-11-05 14:05:21 -0800

    updated file

--- Java/Testing/Testing.java
+++ Java/Testing/Testing.java
@@ -1,5 +1,32 @@
 package Java.Testing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Testing {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String[]> arr = new ArrayList<String[]>();
+
+
+        File f = new File("file.txt");
+
+        Scanner s = new Scanner(f);
+
+        while(s.hasNext())
+        {
+            arr.add(s.nextLine().split(" "));
+        }
+
+        for(String[] sr: arr)
+        {
+            System.out.println(sr[1]);
+        }
+    }
+    
+
+
+
     
 }
