5e82ef3ce4b1643c372c63d4e9f2550e73d3b24c
Drodriguezponce1 <Danieljrod98@yahoo.com>
2024-11-12 15:40:56 -0800

    Unit tests updated for Calculations

--- Java/MOOC/CalculationsTest.java
+++ Java/MOOC/CalculationsTest.java
@@ -2,12 +2,24 @@ package Java.MOOC;
 
 import org.junit.*;
 public class CalculationsTest {
-    
+    public Calculations c = new Calculations();
     @Test
-    public void test1()
+    public void squared_test()
     {
-        Calculations c = new Calculations();
-
         Assert.assertEquals(25, c.squared(5));
     }
+
+    @Test
+    public void square_root_sum_test1()
+    {
+        Assert.assertEquals(1, c.square_root_sum(1, 0));
+    }
+
+    @Test
+    public void square_root_sum_test2()
+    {
+        Assert.assertEquals(3, c.square_root_sum(5, 4));
+    }
+
+
 }
